7c9f44f36d36d845f684f7f5d0f295b04ed7097d
Kariuki11 <irungukenneth433@gmail.com>
2024-07-15 22:27:44 +0300

    new changes

--- myObjects.ts
+++ myObjects.ts
@@ -9,12 +9,16 @@ const User = {
 
 // createUser()
 
+function createCourse():{}{
+    return {}
+}
+
 function createUser({name, isPaid}: {name: string, isPaid: boolean}) {
     // console.log(`User: ${name}, Paid: ${isPaid}`);
   }
   
   // Correct call with an argument
-  createUser({name: "John", isPaid: true});
+  createUser({name: "Ken", isPaid: false});
   
 
 
