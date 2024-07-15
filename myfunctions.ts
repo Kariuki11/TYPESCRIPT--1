5b826b6108fc8fb32daadf1e4ea98d45b7279530
Kariuki11 <irungukenneth433@gmail.com>
2024-07-15 20:40:06 +0300

    New changes

--- myfunctions.ts
+++ myfunctions.ts
@@ -1,5 +1,6 @@
-function addTwo(num){
+function addTwo(num): number{
     return num + 2;
+    //return "hello"
 }
 
 function getUpper(val: string){
@@ -10,11 +11,44 @@ function signUpUser(name: string, email: string, isPaid: boolean){}
 
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {} 
 
-addTwo(5)
+let myValue = addTwo(5)
 getUpper("Ken")
 
 signUpUser("Ken", "ken@254", false)
 loginUser("K", "kk@f.com")
+
+// function getValue(myVal: number): boolean{
+//     if (myVal > 5){
+//         return true
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string):string => {
+    return ""
+}
+
+const heroes = ["Ronald", "sideman", "iranian"]
+//const heroes = [1, 2, 3]
+
+heroes.map(hero => {
+    return `hero is ${hero}`
+})
+
+
+function consoleError(errmsg: string): void{
+    console.log(errmsg);
+
+}
+
+function handleError(errmsg: string): void{
+    console.log(errmsg);
+    
+}
+
+
+
+
 
 
 export {}
