f26cc88a73327415a63a52335a9765306caca1ad
Kariuki11 <irungukenneth433@gmail.com>
2024-07-15 20:13:30 +0300

    New changes

--- myfunctions.js
+++ myfunctions.js
@@ -1,5 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
-    num.toUpperCase();
     return num + 2;
 }
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) { };
 addTwo(5);
+getUpper("Ken");
+signUpUser("Ken", "ken@254", false);
