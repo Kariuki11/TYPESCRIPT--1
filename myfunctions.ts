9178dd9857fe31e6ba4d5cdfb61c870f24f41bac
Kariuki11 <irungukenneth433@gmail.com>
2024-07-15 20:13:15 +0300

    New changes

--- myfunctions.ts
+++ myfunctions.ts
@@ -1,6 +1,19 @@
 function addTwo(num){
-    num.toUpperCase()
     return num + 2;
 }
 
+function getUpper(val: string){
+    return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+let loginUser = (name: string, email: string, isPaid: boolean) => {} 
+
 addTwo(5)
+getUpper("Ken")
+
+signUpUser("Ken", "ken@254", false)
+
+
+export {}
