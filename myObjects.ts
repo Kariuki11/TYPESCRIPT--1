53f91a7310d1afbc7da104633085a36722fce5f5
Kariuki11 <irungukenneth433@gmail.com>
2024-07-16 09:35:24 +0300

    New changes

--- myObjects.ts
+++ myObjects.ts
@@ -1,24 +1,52 @@
-const User = {
-    name: "Ken",
-    email: "K@dev.com",
-    isActive: true
-}
+// const User = {
+//     name: "Ken",
+//     email: "K@dev.com",
+//     isActive: true
+// }
 
-// function createUser({name: string, isPaid: boolean}) {}
+// // function createUser({name: string, isPaid: boolean}) {}
 
 
-// createUser()
+// // createUser()
 
-function createCourse():{}{
-    return {}
-}
+// function createCourse():{}{
+//     return {}
+// }
 
-function createUser({name, isPaid}: {name: string, isPaid: boolean}) {
-    // console.log(`User: ${name}, Paid: ${isPaid}`);
-  }
+// function createUser({name, isPaid}: {name: string, isPaid: boolean}) {
+//     // console.log(`User: ${name}, Paid: ${isPaid}`);
+//   }
   
-  // Correct call with an argument
-  createUser({name: "Ken", isPaid: false});
+//   // Correct call with an argument
+//   createUser({name: "Ken", isPaid: false});
+
+
+//   type User = {
+//     name: string;
+//     email: string;
+//     isPaid: boolean;
+//   }
+
+//   function createUser(user: number){}
+
+
+
+type User = {
+ readonly _id: string
+  name: string
+  email: string
+  isActive: boolean
+  credcardDetails?: number
+}
+
+let myUser: User = {
+  _id: "12345",
+  name: "Ken",
+  email: "K@dev.com",
+  isActive: false
+}
+
+myUser.email =  "kkdev@gmail.com"
   
 
 
